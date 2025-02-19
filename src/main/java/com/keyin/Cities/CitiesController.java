54c2f794b5e660b703eed976dbbede0a2d5018dd
Your Name <youremail@example.com>
2025-02-19 14:19:33 -0330

    Added files for cities controller and service

--- src/main/java/com/keyin/Cities/CitiesController.java
+++ src/main/java/com/keyin/Cities/CitiesController.java
@@ -1,4 +1,33 @@
 package com.keyin.Cities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class CitiesController {
+    @Autowired
+    private CitiesService citiesService;
+
+    @GetMapping("/cities")
+    public List<Cities> getAllCities() {
+        return citiesService.findAllCities();
+    }
+
+    @GetMapping("/cities/{id}")
+    public Cities getCityById(@PathVariable long id) {
+        return citiesService.findCityById(id);
+    }
+
+    @PostMapping("/cities")
+    public Cities createCity(@RequestBody Cities newCity) {
+        return citiesService.createCity(newCity);
+    }
+
+    @PutMapping("/cities/{id}")
+    public Cities updateCity(@PathVariable long id, @RequestBody Cities updatedCity) {
+        return citiesService.updateCity(id, updatedCity);
+    }
 }
--- src/main/java/com/keyin/Cities/CitiesService.java
+++ src/main/java/com/keyin/Cities/CitiesService.java
@@ -1,4 +1,41 @@
 package com.keyin.Cities;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class CitiesService {
+    @Autowired
+    private CitiesRepository cityRepository;
+
+    public List<Cities> findAllCities() {
+        return (List<Cities>) cityRepository.findAll();
+    }
+
+    public Cities findCityById(long id) {
+        Optional<Cities> optionalCity = cityRepository.findById(id);
+        return optionalCity.orElse(null);
+    }
+
+    public Cities createCity(Cities newCity) {
+        return cityRepository.save(newCity);
+    }
+
+    public Cities updateCity(long id, Cities updatedCity) {
+        Cities cityToUpdate = findCityById(id);
+
+        if (cityToUpdate != null) {
+            cityToUpdate.setName(updatedCity.getName());
+            cityToUpdate.setState(updatedCity.getState());
+            cityToUpdate.setPopulation(updatedCity.getPopulation());
+
+            return cityRepository.save(cityToUpdate);
+        }
+
+        return null;
+    }
 }
+
