f0c85f97af280c6dbbc81646e15b23d03f7ed593
Your Name <youremail@example.com>
2025-02-19 14:19:10 -0330

    Added files for all controllers and services

--- src/main/java/com/keyin/Airport/Airport.java
+++ src/main/java/com/keyin/Airport/Airport.java
@@ -16,31 +16,37 @@ public class Airport {
     private String code;
 
     public long getId() {
+
         return id;
     }
 
 
     public void setId(long id) {
+
         this.id = id;
     }
 
 
     public String getName() {
+
         return name;
     }
 
 
     public void setName(String name) {
+
         this.name = name;
     }
 
 
     public String getCode() {
+
         return code;
     }
 
 
     public void setCode(String code) {
+
         this.code = code;
     }
 }
--- src/main/java/com/keyin/Airport/AirportService.java
+++ src/main/java/com/keyin/Airport/AirportService.java
@@ -1,4 +1,41 @@
 package com.keyin.Airport;
 
+import com.keyin.Cities.CitiesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class AirportService {
+    @Autowired
+    private AirportRepository airportRepository;
+
+    public List<Airport> findAllAirports() {
+        return (List<Airport>) airportRepository.findAll();
+    }
+
+    public Airport findAirportById(long id) {
+        Optional<Airport> optionalAirport = airportRepository.findById(id);
+        return optionalAirport.orElse(null);
+    }
+
+    public Airport createAirport(Airport newAirport) {
+        return airportRepository.save(newAirport);
+    }
+
+    public Airport updateAirport(long id, Airport updatedAirport) {
+        Airport airportToUpdate = findAirportById(id);
+
+        if (airportToUpdate != null) {
+            airportToUpdate.setName(updatedAirport.getName());
+            airportToUpdate.setCode(updatedAirport.getCode());
+            airportToUpdate.setId(updatedAirport.getId());
+
+            return airportRepository.save(airportToUpdate);
+        }
+
+        return null;
+    }
 }
