656c694c9dd4e9484ca7b9156ea831f7dd7031ee
Your Name <youremail@example.com>
2025-02-19 14:19:53 -0330

    Added files for passenger controller and service

--- src/main/java/com/keyin/Passengers/PassengersController.java
+++ src/main/java/com/keyin/Passengers/PassengersController.java
@@ -1,4 +1,33 @@
 package com.keyin.Passengers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
 public class PassengersController {
+    @Autowired
+    private PassengersService passengersService;
+
+    @GetMapping("/passengers")
+    public List<Passengers> getAllPassengers() {
+        return passengersService.findAllPassengers();
+    }
+
+    @GetMapping("/passengers/{id}")
+    public Passengers getPassengerById(@PathVariable long id) {
+        return passengersService.findPassengerById(id);
+    }
+
+    @PostMapping("/passengers")
+    public Passengers createPassenger(@RequestBody Passengers newPassenger) {
+        return passengersService.createPassenger(newPassenger);
+    }
+
+    @PutMapping("/passengers/{id}")
+    public Passengers updatePassenger(@PathVariable long id, @RequestBody Passengers updatedPassenger) {
+        return passengersService.updatePassenger(id, updatedPassenger);
+    }
 }
--- src/main/java/com/keyin/Passengers/PassengersService.java
+++ src/main/java/com/keyin/Passengers/PassengersService.java
@@ -1,4 +1,46 @@
 package com.keyin.Passengers;
 
+import com.keyin.Cities.Cities;
+import com.keyin.Cities.CitiesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class PassengersService {
+    @Autowired
+    private PassengersRepository passengersRepository;
+
+    @Autowired
+    private CitiesService citiesService;
+
+    public List<Passengers> findAllPassengers() {
+        return (List<Passengers>) passengersRepository.findAll();
+    }
+
+    public Passengers findPassengerById(long id) {
+        Optional<Passengers> optionalPassenger = passengersRepository.findById(id);
+        return optionalPassenger.orElse(null);
+    }
+
+    public Passengers createPassenger(Passengers newPassenger) {
+        return passengersRepository.save(newPassenger);
+    }
+
+    public Passengers updatePassenger(long id, Passengers updatedPassenger) {
+        Passengers passengerToUpdate = findPassengerById(id);
+
+        if (passengerToUpdate != null) {
+            passengerToUpdate.setFirstName(updatedPassenger.getFirstName());
+            passengerToUpdate.setLastName(updatedPassenger.getLastName());
+            passengerToUpdate.setPhoneNumber(updatedPassenger.getPhoneNumber());
+            passengerToUpdate.setId(updatedPassenger.getId());
+
+            return passengersRepository.save(passengerToUpdate);
+        }
+
+        return null;
+    }
 }
