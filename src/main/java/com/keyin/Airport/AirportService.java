3f51e16cccb4900378ddab3babaa45dbd3a742ba
Your Name <youremail@example.com>
2025-03-03 17:30:25 -0330

    Updated files to make List airports used by passengers work

--- src/main/java/com/keyin/Airport/AirportService.java
+++ src/main/java/com/keyin/Airport/AirportService.java
@@ -9,14 +9,19 @@ import java.util.List;
 public class AirportService {
     @Autowired
     private AirportRepository airportRepository;
-    // Gets list of airports ny city id
+
+    // Get list of airports by city ID
     public List<Airport> getAirportsByCityId(Long cityId) {
         return airportRepository.findByCityId(cityId);
     }
 
+    // Create a new airport
     public Airport createAirport(Airport airport) {
         return airportRepository.save(airport);
     }
 
-
+    // Get all airports
+    public List<Airport> getAllAirports() {
+        return airportRepository.findAll();
+    }
 }
